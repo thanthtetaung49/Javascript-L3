4d428a776ace1eca414e0293442a121d3caf01f5
Thant Htet Aung <93987523+thanthtetaung49@users.noreply.github.com>
2022-11-04 14:49:11 +0630

    2:49pm commit

--- 20_texteditor/app.js
+++ 20_texteditor/app.js
@@ -1,45 +1,88 @@
 var getTextArea = document.getElementById("textarea");
+var getDivArea = document.getElementById("div-area");
 
-function boldFun() {
-    getTextArea.style.fontWeight = "bold";
-};
+getDivArea.contentEditable = true;
+getDivArea.spellcheck = false;
 
-function italicFun() {
-    getTextArea.style.fontStyle = "italic";
-};
+var getButtons = document.querySelectorAll(".btn");
+// console.log(getButtons); // NodeList
 
-function underlineFun() {
-    getTextArea.style.textDecoration = "underline";
-};
+getButtons.forEach(function (getButton) {
+    getButton.addEventListener("click", function () {
+        // console.log("hi");
 
-function leftFun() {
-    getTextArea.style.textAlign = "left";
-};
+        // var getCommand = getButton.getAttribute("data-command");
+        var getCommand = getButton.dataset["command"];
+        // console.log(getCommand);
 
-function centerFun() {
-    getTextArea.style.textAlign = "center";
-};
+        if (getCommand === "clearText") {
+            getDivArea.innerHTML = "";
+        } else if (getCommand === "createLink" || getCommand === "insertImage") {
+                                // message                  default
+            let getUrl = prompt("Enter your website link", "https://");
+            document.execCommand(getCommand, false, getUrl);
+        } else if (getCommand === "foreColor") {
+            // console.log(getButton.value);
+            document.execCommand(getCommand, false, getButton.value);
+        } else if (getCommand === "backColor") {
+            document.execCommand(getCommand, false, getButton.value);
+        } else if (getCommand === "fontName") {
+            document.execCommand(getCommand, false, getButton.value);
+        } else if (getCommand === "paste") {
+            navigator.clipboard.readText().then(function (clipText) {
+                // console.log(clipText);
+                getDivArea.innerHTML += clipText;
+            });
+        } else {
+            document.execCommand(getCommand, false, null);
+        };
+    });
+});
 
-function rightFun() {
-    getTextArea.style.textAlign = "right";
-};
+// function boldFun() {
+//     getTextArea.style.fontWeight = "bold";
+// };
 
-function upperFun() {
-    getTextArea.style.textTransform = "uppercase";
-};
+// function italicFun() {
+//     getTextArea.style.fontStyle = "italic";
+// };
 
-function lowerFun() {
-    getTextArea.style.textTransform = "lowercase";
-};
+// function underlineFun() {
+//     getTextArea.style.textDecoration = "underline";
+// };
 
-function capitalizeFun() {
-    getTextArea.style.textTransform = "capitalize";
-};
+// function leftFun() {
+//     getTextArea.style.textAlign = "left";
+// };
 
-function clearFun() {
-    getTextArea.style.fontWeight = "normal";
-    getTextArea.style.fontStyle = "normal";
-    getTextArea.style.textDecoration = "none";
-    getTextArea.style.textAlign = "left";
-    getTextArea.value = "";
-};
+// function centerFun() {
+//     getTextArea.style.textAlign = "center";
+// };
+
+// function rightFun() {
+//     getTextArea.style.textAlign = "right";
+// };
+
+// function upperFun() {
+//     getDivArea.style.textTransform = "uppercase";
+// };
+
+// function lowerFun() {
+//     getDivArea.style.textTransform = "lowercase";
+// };
+
+// function capitalizeFun() {
+//     getDivArea.style.textTransform = "capitalize";
+// };
+
+// function clearFun() {
+//     getTextArea.style.fontWeight = "normal";
+//     getTextArea.style.fontStyle = "normal";
+//     getTextArea.style.textDecoration = "none";
+//     getTextArea.style.textAlign = "left";
+//     getTextArea.value = "";
+// };
+
+// execCommand(aCommandName, aShowDefaultUI, Argument);
+
+// aShowDefaultUI => true, false
